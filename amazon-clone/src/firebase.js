76fd0633fc46e2c62094d452ec969803d46d6585
khairatunhissan <khairatunhissan@gmail.com>
2021-07-20 10:16:27 +0600

    amazon setup and navbar

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBr94C7fEDmtm23U0CPNImXlj0JrDJteQM",
+    authDomain: "clone-29144.firebaseapp.com",
+    projectId: "clone-29144",
+    storageBucket: "clone-29144.appspot.com",
+    messagingSenderId: "861931912372",
+    appId: "1:861931912372:web:40170b4c2637c3d2da2be1",
+    measurementId: "G-KJZR72YBTK"
+  };
